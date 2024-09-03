914b3f77e6fb99d9e3ca8ff9ce122a6fcbf0b389
Tintin0013 <arnaudpinatel@arnauds-MacBook-Pro.local>
2024-09-03 13:34:35 +0200

    ajout de la bdd

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Band(models.Model):
+    name = models.fields.CharField(max_length=100)
+
+class Listing(models.Model):
+    title = models.fields.CharField(max_length=100)
