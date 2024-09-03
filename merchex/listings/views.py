9bc91bedf28a4e6b20291371a7364c2f0d1aa71d
Tintin0013 <arnaudpinatel@arnauds-MacBook-Pro.local>
2024-09-03 14:21:55 +0200

    ajout des templates

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,33 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from listings.models import Band
-
+from listings.models import Band, Listing
 
 def hello(request):
-    # return HttpResponse('<h1>Hello Django!</h1>')
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-""")
+    return render(request, 'listings/hello.html',{'bands': bands})
     
 
 def about(request):
-    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+    #return HttpResponse('<h1>À propos</h1> <p>Nous adorons Le soleil</p>')
+    return render(request,
+                  'listings/about.html')
 
 def contact(request):
-    return HttpResponse('<h1>Pour nous contacter:</h1><p>Cliquez ICI</p>')
-    # return render(request,
-    #               'listings/contact.html')
+    #return HttpResponse('<h1>Pour nous contacter:</h1><p>Cliquez ICI</p>')
+    return render(request,
+                  'listings/contact.html')
 
 def listings(request):
-    # listings = Listing.objects.all()
-    return HttpResponse('<h1>Voici la liste des Produits Disponibles</h1><p>choisir un produit</p>')
-    # return render(request,
-    #               'listings/listings.html',
-    #               context={'listings': listings})
+    listings = Listing.objects.all()
+   # return HttpResponse('<h1>Voici la liste des Produits Disponibles</h1><p>choisir un produit</p>')
+    return render(request,
+                  'listings/listings.html',
+                  context={'listings': listings})
