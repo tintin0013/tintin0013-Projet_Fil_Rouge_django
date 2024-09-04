088e32529877865e8eff657243262b56c2dd3cf2
Tintin0013 <arnaudpinatel@arnauds-MacBook-Pro.local>
2024-09-04 10:35:06 +0200

    mise a jour pour l'utilisation du site administration

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from listings.models import Band, Listing
 
 # Register your models here.
+# listings/admin.py
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'sold', 'year','band')
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,40 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+    class Genre(models.TextChoices):
+        ROCK = 'RK'
+        POP = 'PP'
+        JAZZ = 'JZ'
+        CLASSICAL = 'CL'
+        HIP_HOP = 'HH'
+        INSTRUMENTAL = 'IN'
+        REGGAE = 'RE'
+        
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2024)])
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+    like_new = models.fields.BooleanField(default=False)
+    def __str__(self):
+        return f'{self.name}'
 
 class Listing(models.Model):
+
+    class Type(models.TextChoices):
+        DISQUE = 'CD'
+        VETEMENT = 'VE'
+        POSTERS = 'P'
+        DIVERS = 'DI'
+        
+    
     title = models.fields.CharField(max_length=100)
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(null=True, blank=True)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
